10c8104440c75453f7ea7fee967d2cda955be411
구환준/모건 <koojun99@naver.com>
2024-07-27 18:38:48 +0900

    프로젝트 시작

---
+++ src/main/java/server/rebid/ReBidApplication.java
@@ -0,0 +1,13 @@
+package server.rebid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReBidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReBidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/server/rebid/ReBidApplicationTests.java
@@ -0,0 +1,13 @@
+package server.rebid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReBidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
