8e5848ee0e4c62f3477d64b9163905ac83c3d474
구환준/모건 <koojun99@naver.com>
2024-07-29 18:21:07 +0900

    Feat: 이미지 관련 세팅

--- src/main/java/server/rebid/common/config/SwaggerConfig.java
+++ src/main/java/server/rebid/common/config/SwaggerConfig.java
@@ -1,10 +1,7 @@
-package server.rebid.config;
+package server.rebid.common.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
