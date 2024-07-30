e7fbfde59d33982c9b1e677fda96178b8870629b
구환준/모건 <koojun99@naver.com>
2024-07-31 03:29:16 +0900

    쿠키 도메인 설정 변경

--- src/main/java/server/rebid/auth/service/CookieService.java
+++ src/main/java/server/rebid/auth/service/CookieService.java
@@ -23,6 +23,8 @@ public class CookieService {
         ResponseCookie cookie = ResponseCookie.from(name, value)
                 .maxAge(maxAge)
                 .path("/")
+                .domain(domain)
+                .httpOnly(false)
                 .secure(true)
                 .sameSite("None")
                 .build();
