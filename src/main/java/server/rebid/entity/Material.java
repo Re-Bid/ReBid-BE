12c9f075ef4c0a6e2ca925ae7d7a7f0ce5c412b2
jorippppong <duelee75@gmail.com>
2024-07-31 07:17:12 +0900

    재료 엔티티 추가

---
+++ src/main/java/server/rebid/entity/Material.java
@@ -0,0 +1,29 @@
+package server.rebid.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Material {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "material", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ItemImage> itemImages = new ArrayList<>();  // 상품 이미지 URL
+
+}
