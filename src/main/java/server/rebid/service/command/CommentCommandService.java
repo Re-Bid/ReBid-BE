a40f64a8ebd4b64c9aa83aec2afb946b83f3f9d7
jorippppong <duelee75@gmail.com>
2024-07-31 18:17:00 +0900

    댓글 조회 서비스 추가

--- src/main/java/server/rebid/service/command/CommentCommandService.java
+++ src/main/java/server/rebid/service/command/CommentCommandService.java
@@ -3,6 +3,6 @@ package server.rebid.service.command;
 import server.rebid.dto.response.CommentResponse;
 
 public class CommentCommandService {
-    public CommentResponse.CommentId addComment(Long materialId, String content) {
+    public CommentResponse.CommentId addComment(Long memberId, Long materialId, String content) {
     }
 }
